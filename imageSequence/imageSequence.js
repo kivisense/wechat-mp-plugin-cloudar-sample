f00cce8875ec3f5c2220a8261626b43ab417221d
listenlin <listenlin521@foxmail.com>
2021-06-02 18:20:20 +0800

    优化代码

--- imageSequence/imageSequence.js
+++ imageSequence/imageSequence.js
@@ -3,55 +3,59 @@ Component({
     fps: {
       type: Number,
       default: 60,
-    }
+    },
   },
   async ready() {
     const giftLoading = [];
     for (let i = 0; i <= 74; i++) {
-      const name = `${i}`.padStart(5, '0');
+      const name = `${i}`.padStart(5, "0");
       giftLoading.push(`../images/RedPacket/RedPacket_${name}.png`);
     }
     try {
-      const { width: canvasWidth, height: canvasHeight } = await new Promise((r) => {
-        const query = this.createSelectorQuery();
-        query.select('#sequence').boundingClientRect(r);
-        query.exec();
-      })
+      const { width: canvasWidth, height: canvasHeight } = await new Promise(
+        (r) => {
+          const query = this.createSelectorQuery();
+          query.select(".sequence").boundingClientRect(r);
+          query.exec();
+        },
+      );
 
-      const { width: imageWidth, height: imageHeight } =  await new Promise((resolve, reject)=>{
-        wx.getImageInfo({
-          src: giftLoading[0],
-          success (res) {
-            resolve(res)
-          },
-          fail(e){
-            reject(e)
-          }
-        })
-      })
-      const cxt = wx.createCanvasContext('image-sequence', this);
-        let frameIndex = 0;
-        const render = () => {
-          if (frameIndex >= giftLoading.length) {
-            frameIndex = 0;
-          }
-          cxt.drawImage(
-            giftLoading[frameIndex++],
-            0, 0,
-            imageWidth, imageHeight,
-            0, 0,
-            canvasWidth, canvasHeight,
-          );
-          cxt.draw(false);
-          setTimeout(render, 1000 / this.data.fps);
-        };
-        render();
+      const { width: imageWidth, height: imageHeight } = await new Promise(
+        (resolve, reject) => {
+          wx.getImageInfo({
+            src: giftLoading[0],
+            success(res) {
+              resolve(res);
+            },
+            fail(e) {
+              reject(e);
+            },
+          });
+        },
+      );
+      const cxt = wx.createCanvasContext("image-sequence", this);
+      let frameIndex = 0;
+      const render = () => {
+        if (frameIndex >= giftLoading.length) {
+          frameIndex = 0;
+        }
+        cxt.drawImage(
+          giftLoading[frameIndex++],
+          0,
+          0,
+          imageWidth,
+          imageHeight,
+          0,
+          0,
+          canvasWidth,
+          canvasHeight,
+        );
+        cxt.draw(false);
+        setTimeout(render, 1000 / this.data.fps);
+      };
+      render();
     } catch (error) {
       console.error(error);
     }
   },
-  detached(){
-
-  },
-  methods: {}
-})
+});
--- index/index.js
+++ index/index.js
@@ -1,24 +1,45 @@
-const cloudar = requirePlugin("cloudar");
+const { startCloudar, stopCloudar } = requirePlugin("kivicube");
 
 Page({
   data: {
     result: "",
-    giftVisibility: false
+    giftVisibility: false,
   },
   async start() {
-    // this.setData({'giftVisibility':true})
-    const { identifier, metadata } = await cloudar.startCloudar();
-    const { data: { projectId, sceneId } } = JSON.parse(metadata);
-    this.setData({'giftVisibility':true})
-    const result = JSON.stringify({
-      collectionId: projectId,
-      identifier,
-      sceneId,
-    }, null, "\t");
-    this.setData({ result });
+    if (this._isStart) {
+      return;
+    }
+    this._isStart = true;
+
+    try {
+      wx.showNavigationBarLoading();
+      const { identifier, metadata } = await startCloudar();
+
+      const {
+        data: { projectId, sceneId },
+      } = JSON.parse(metadata);
+
+      this.setData({ giftVisibility: true });
+
+      const result = JSON.stringify(
+        {
+          collectionId: projectId,
+          identifier,
+          sceneId,
+        },
+        null,
+        "\t",
+      );
+      this.setData({ result });
+    } catch (e) {
+      console.error("云识别失败", e);
+    } finally {
+      wx.hideNavigationBarLoading();
+      this._isStart = false;
+    }
   },
   stop() {
-    cloudar.stopCloudar();
+    stopCloudar();
   },
   error(e) {
     if (e && e.detail && e.detail.detail && e.detail.detail.errMsg) {
@@ -31,15 +52,18 @@ Page({
             wx.openSetting({
               success({ authSetting: { "scope.camera": isGrantedCamera } }) {
                 if (isGrantedCamera) {
-                  wx.redirectTo({ url: '/' + page.__route__ });
+                  wx.redirectTo({ url: "/" + page.__route__ });
                 } else {
-                  wx.showToast({ title: "获取“摄像头”权限失败！", icon: "none" });
+                  wx.showToast({
+                    title: "获取“摄像头”权限失败！",
+                    icon: "none",
+                  });
                 }
-              }
+              },
             });
-          }
+          },
         });
       }
     }
-  }
-})
+  },
+});
